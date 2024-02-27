433c7943a5f70c1f81ec891a2d69c83107a804f7
metamaru <kmeg.328@gmail.com>
2024-02-27 15:29:09 +0900

    新規投稿画面の表示

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,25 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
+    @event = Event.new
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to root_path
+    else
+      render :new, atatus: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :comment, :location, :start_time, :end_time, :all_day, :category_id).merge(user_id: current_user.id)
   end
 end
