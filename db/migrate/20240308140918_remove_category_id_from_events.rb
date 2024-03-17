5d23218cd18cc75daa473e24fe87fd4a9b7388d7
metamaru <kmeg.328@gmail.com>
2024-03-17 19:03:27 +0900

    Update 20240308140918_remove_category_id_from_events.rb

--- db/migrate/20240308140918_remove_category_id_from_events.rb
+++ db/migrate/20240308140918_remove_category_id_from_events.rb
@@ -1,5 +1,5 @@
 class RemoveCategoryIdFromEvents < ActiveRecord::Migration[7.0]
   def change
-    remove_column :events, :category, foreign_key: true
+    remove_column :events, :category, :string
   end
 end
