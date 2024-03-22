5aff25893d61edddaeaff5974820b2a99fecae57
metamaru <kmeg.328@gmail.com>
2024-03-22 16:12:36 +0900

    allDayの修正

--- app/javascript/allDay.js
+++ app/javascript/allDay.js
@@ -1,14 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
   const allDayCheckbox = document.getElementById("all_day_checkbox");
-  const startDateInput = document.getElementById("start_time");
-  const endDateInput = document.getElementById("end_time");
+  const startDateInput = document.getElementById("#event_start_time_1i, #event_start_time_2i, #event_start_time_3i, #event_start_time_4i, #event_start_time_5i");
+  const endDateInput = document.getElementById("#event_end_time_1i, #event_end_time_2i, #event_end_time_3i, #event_end_time_4i, #event_end_time_5i");
 
   allDayCheckbox.addEventListener("change", function() {
     if (allDayCheckbox.checked) {
-      endDateInput.value = startDateInput.value;
-      endDateInput.disabled = true;
+      // 開始時刻のセレクト要素の値を取得
+      const startDateValue = Array.from(startDateSelects).map(select => select.value).join("-");
+      // 終了時刻のセレクト要素に値をセット
+      endDateSelects.forEach(select => {
+        select.value = startDateValue;
+        select.disabled = true;
+      });
     } else {
-      endDateInput.disabled = false;
+      endDateSelects.forEach(select => {
+        select.disabled = false;
+      });
     }
   });
 });
